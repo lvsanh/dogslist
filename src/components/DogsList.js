0d498f8bafab3ecc2da8ff7f940fef6e5e351f2e
lvsanh <lvsanh@dtvt.org>
2018-10-31 12:23:56 +0100

    done

--- src/components/DogsList.js
+++ src/components/DogsList.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import * as request from 'superagent'
 
 export default class Dogslist extends Component {
-    state = { dogBreeds: [] }
+    state = { dogBreeds: null }
 
     // componentDidMount() {
     //     request
@@ -10,17 +10,17 @@ export default class Dogslist extends Component {
     //         .then(response => console.log(Object.keys(response.body.message)))
     //         .catch(console.error)
     // }
+    updateBreeds(breeds) {
+        this.setState({
+            dogBreeds: breeds
+        })
+    }
+
     componentDidMount() {
         request
             .get('https://dog.ceo/api/breeds/list/all')
             .then(response => this.updateBreeds(Object.keys(response.body.message)))
             .catch(console.error)
-    }
-
-    updateBreeds(breeds) {
-        this.setState({
-            dogBreeds: breeds
-        })
     }
 
     render() {
@@ -29,7 +29,7 @@ export default class Dogslist extends Component {
                 <h1>Dogs List</h1>
                 { /*this.state.dogBreeds === null && 'Loading...'*/}
                 {<ul>
-                    {this.state.dogBreeds.map(breed => <li key={breed}>{breed}</li>)}
+                    {this.state.dogBreeds !== null && this.state.dogBreeds.map(breed => <li key={breed}>{breed}</li>)}
                 </ul>}
             </div>
         )
