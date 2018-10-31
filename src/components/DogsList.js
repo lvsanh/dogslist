1b82cfcda689b5f0bce6f2b4bbbcf353d77b9b26
lvsanh <lvsanh@dtvt.org>
2018-10-31 10:58:06 +0100

    fetch and transfrom dogs list

--- src/components/DogsList.js
+++ src/components/DogsList.js
@@ -1,10 +1,27 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
+import * as request from 'superagent'
 
 export default class Dogslist extends Component {
-    render (){
+    state = { dogBreeds: null }
+
+    // componentDidMount() {
+    //     request
+    //         .get('https://dog.ceo/api/breeds/list/all')
+    //         .then(console.log)
+    //         .catch(console.error)
+    // }
+    componentDidMount() {
+        request
+            .get('https://dog.ceo/api/breeds/list/all')
+            .then(response => console.log(Object.keys(response.body.message)))
+            .catch(console.error)
+    }
+
+    render() {
         return (
-            <div className = "dogs-list">
+            <div className="dogs-list">
                 <h1>Dogs List</h1>
+                {this.state.dogBreeds === null && 'Loading...'}
             </div>
         )
     }
